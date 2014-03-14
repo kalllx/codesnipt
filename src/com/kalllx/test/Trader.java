e20b9ca2746cefc33bb1f74bb31af520791820f4
kalllx <kalllx@yahoo.cn>
2014-03-14 16:19:15 +0800

    Signed-off-by: kalllx <kalllx@yahoo.cn>

--- src/com/kalllx/test/Trader.java
+++ src/com/kalllx/test/Trader.java
@@ -3,6 +3,19 @@ package com.kalllx.test;
 public class Trader extends Thread
 {
     
+    private Category category;
+   
+    public void executeCategory()
+    {
+	
+    }
+    
+    public void stopExecute(){
+	
+    }
+    
+    
+    
     @Override
     public void run()
     {
